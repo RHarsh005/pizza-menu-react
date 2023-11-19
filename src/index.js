9ae718ce2798ec5152d8c4daae4518553d970fa3
Rupani Harsh Vallabhbhai <Harsh.Rupani1@ibm.com>
2023-11-19 13:02:59 +0530

    added final working code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import pizzaData from "./data";
+// console.log(pizzaData);
+// console.log(typeof pizzaData);
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 style={{ color: "" }}>PIZZA MENU</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  //   for (let d : JSON.parse(data)) {
+  //     console.log(d);
+  //   }
+  let length = pizzaData.length;
+  //   pizzaData.forEach((pizza) => console.log(pizza));
+  return (
+    <main className="menu">
+      <h2> Our Menu </h2>
+
+      {length > 0 && (
+        <>
+          <p>
+            A pizzeria is a restaurant focusing on pizza. As well as pizza, dishes at pizzerias can
+            include kebab, salads and pasta.{" "}
+          </p>
+
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      )}
+      {/* <Pizza
+        name="Focaccia"
+        ingredients="Bread with italian olive oil and rosemary"
+        photoName="pizzas/focaccia.jpg"
+        price="50"
+      /> */}
+    </main>
+  );
+}
+
+function Pizza({ pizzaObj }) {
+  //   console.log(test);
+
+  //   if (pizzaObj.soldOut) return null;
+  return (
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <div>
+        <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+        <h3> {pizzaObj.name} </h3>
+        <p> {pizzaObj.ingredients} </p>
+        <span> {pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price} </span>
+      </div>
+    </li>
+  );
+}
+
+function Footer() {
+  let hour = new Date().getHours();
+  let open = 12,
+    close = 18;
+  let openFlag = open <= hour && hour <= close;
+  return (
+    <footer className="footer">
+      We are currently {openFlag ? "Open" : "Close"} <Order />
+    </footer>
+  );
+  //   return React.createElement("footer", null, " ---- FOOTER ---- ");
+}
+
+function Order() {
+  return (
+    <div className="order">
+      <button className="btn">Order</button>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
